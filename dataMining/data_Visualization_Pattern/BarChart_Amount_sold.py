604dd9bcdfdabc1bde38a53a333d5099c787f4b7
Chris <cchriscclr@gmail.com>
2019-04-21 11:01:44 -0400

    Create BarChart_Amount_sold.py

---
+++ dataMining/data_Visualization_Pattern/BarChart_Amount_sold.py
@@ -0,0 +1,23 @@
+#include the total amount sold, electronics and furniture sold
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import collections 
+
+dataSet = pd.read_excel('Sales.xlsx', index_col=0)
+
+myNpArray = np.array(dataSet)
+
+
+col_data = myNpArray[:, 8]
+
+c = collections.Counter(col_data)
+
+fig = plt.figure(1, figsize= (9,2))
+ax = fig.add_subplot(132)
+ax.set_title("Amount Sold")
+plt.bar(list(c.keys())[0],list(c.values())[0])
+plt.bar(list(c.keys())[1],list(c.values())[1])
+plt.show()
+
